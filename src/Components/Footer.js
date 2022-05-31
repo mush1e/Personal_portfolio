400685afbcf8c25dcfe5bc532261ce9199a328e3
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2022-05-31 12:28:42 -0400

    Rotated email div

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,10 +3,10 @@ import "../styles/footer.css"
 export default function Footer () {
     return(
         <div className="footer--div">
-            <div className="icon--footer-div">
+            <div className="email--footer-div">
                 <text>mustafa.a.r.siddiqui@outlook.com</text>
             </div>
-            <div className="email--footer-div">
+            <div className="icon--footer-div">
                 <span></span>
                 <text>links go here</text>
             </div>
